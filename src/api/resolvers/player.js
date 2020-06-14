7753b9d00989aac5dcf5b454f02d86150e4b647c
Trikolon <git@pbz.im>
2020-06-14 20:11:01 +0200

    added 'activeServerBans' player field

--- src/api/resolvers/player.js
+++ src/api/resolvers/player.js
@@ -1,18 +1,22 @@
 const { GraphQLID } = require('graphql');
+const playerTypeConstructor = require('../types/player');
 
-const Player = require('../types/player');
+module.exports = (dataSources) => {
+  const { bungeeAdminTools } = dataSources;
+  const Player = playerTypeConstructor(dataSources);
 
-module.exports = ({ bungeeAdminTools }) => ({
-  type: Player,
-  args: {
-    uuid: {
-      type: GraphQLID,
+  return {
+    type: Player,
+    args: {
+      uuid: {
+        type: GraphQLID,
+      },
     },
-  },
-  resolve(_, args) {
-    if (!args.uuid) {
-      throw new Error('UUID is mandatory');
-    }
-    return bungeeAdminTools.models.player.findByPk(args.uuid);
-  },
-});
+    resolve(_, args) {
+      if (!args.uuid) {
+        throw new Error('UUID is mandatory');
+      }
+      return bungeeAdminTools.models.player.findByPk(args.uuid);
+    },
+  };
+};
--- src/api/types/player.js
+++ src/api/types/player.js
@@ -1,6 +1,6 @@
-const { GraphQLObjectType, GraphQLString } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLList } = require('graphql');
 
-module.exports = new GraphQLObjectType({
+module.exports = ({ bungeeAdminTools }) => new GraphQLObjectType({
   name: 'player',
   description: 'player',
   fields() {
@@ -20,6 +20,19 @@ module.exports = new GraphQLObjectType({
       firstLogin: {
         type: GraphQLString,
         description: 'Timestamp of first ever login',
+      },
+      activeServerBans: {
+        type: GraphQLList(GraphQLString),
+        description: 'Array of names of servers, which player is banned from',
+        async resolve(player) {
+          const queryResult = await bungeeAdminTools.models.ban.findAll({
+            attributes: ['banServer'],
+            where: {
+              uuid: player.uuid, banState: 1,
+            },
+          });
+          return queryResult.map((el) => el.dataValues?.banServer).filter((el) => el != null);
+        },
       },
     };
   },
