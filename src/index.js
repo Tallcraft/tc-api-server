20e0ac9617511c5ef67b56a738d746fc0636e988
Trikolon <git@pbz.im>
2020-06-15 19:43:35 +0200

    Updated example config, actually use apollo config

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const { ApolloServer } = require('apollo-server');
 const { resolvers } = require('./resolvers');
 const { typeDefs } = require('./typeDefs');
 
+const config = require('../config.json');
+
 (async () => {
   const server = new ApolloServer({ typeDefs, resolvers });
 
-  const { url } = await server.listen();
+  const { url } = await server.listen(config.graphql);
   console.info('Started server at', url);
 })();
