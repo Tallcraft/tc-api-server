7281bf711a22757ccfa14e084e9ca657d237c3f8
Trikolon <git@pbz.im>
2020-06-18 19:53:10 +0200

    Removed obsolete migration files.

--- src/migrateme/schema.js
+++
@@ -1,9 +0,0 @@
-const { GraphQLObjectType, GraphQLSchema } = require('graphql');
-const resolvers = require('./resolvers');
-
-module.exports = (dataSources) => new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'Query',
-    fields: resolvers(dataSources),
-  }),
-});
