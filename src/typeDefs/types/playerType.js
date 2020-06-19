07644d92f0fdaba5dee9f04256f27622dbdb23a1
Trikolon <git@pbz.im>
2020-06-19 16:44:17 +0200

    Added Documentation for player type fields.

--- src/typeDefs/types/playerType.js
+++ src/typeDefs/types/playerType.js
@@ -2,10 +2,15 @@ const { gql } = require('apollo-server');
 
 const playerType = gql`
     type Player {
+        # Global unique identifier
         uuid: ID!
+        # Current player name, may be unknown.
         name: String,
+        # Last name of the player the server saw. May be outdated or unset.
         lastSeenName: String
+        # UNIX timestamp of last player login.
         lastLogin: String
+        # Unix timestamp of first player login on any server of the network.
         firstLogin: String
         infractions: PlayerInfractions
     }
