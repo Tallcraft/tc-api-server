5338b625ae647463d481086483e83b7796ed2390
Metype <39036543+metype@users.noreply.github.com>
2024-07-07 00:16:52 -0700

    Make the API able to take an array for name matching (#50)

--- src/models/Player.js
+++ src/models/Player.js
@@ -6,8 +6,16 @@ const { isNullUUID, fixUpUUID, stripUUID } = require('./util');
 const { Op } = Sequelize;
 
 class Player {
+ /**
+ * Returns a list of results from a players query in result and the number of total matches in totalCount.
+ * @param {number} limit - Max number of results to return.
+ * @param {number} offset - Number to offset the first index of results by, useful for pagination.
+ * @param {string} order - ASC or DESC, determines the order of players returned in result ordered by first joined date.
+ * @param {boolean} [matchAll=true] - If true; searchPlayerName's elements all should match lastSeenName to be returned. If false, any can match.
+ * @param {string[]} [searchPlayerName=[]] - List of strings to use for filtering by lastSeenName 
+ */
   static async getPlayerList({
-    limit, offset, order, searchPlayerName,
+    limit, offset, order, matchAll, searchPlayerName,
   }) {
     const options = {
       offset,
@@ -16,12 +24,22 @@ class Player {
     };
 
     if (searchPlayerName) {
-      options.where = {
-        lastSeenName: Sequelize.where(
-          Sequelize.fn('LOWER',
-            Sequelize.col('BAT_player')), 'LIKE', searchPlayerName,
-        ),
-      };
+      const nameMatchElements = searchPlayerName.map((nameElement) => ({
+        lastSeenName: {
+          [Op.like]: nameElement,
+        },
+      }));
+
+      // And forces all to match, Or allows any to match
+      if (matchAll) {
+        options.where = {
+          [Op.and]: nameMatchElements,
+        };
+      } else {
+        options.where = {
+          [Op.or]: nameMatchElements,
+        };
+      }
     }
     const playerQuery = bungeeAdminToolsConnector.models.player.findAll(options)
       // Fix invalid UUIDS without dashes
